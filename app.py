8c648416e46bbe082d52868a3c10e4398712ec8c
c04o <c04o@proton.me>
2025-05-06 11:36:19 -0600

    Lógica del servidor y rutas de interacción con la pila agregadas a la aplicación

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
+from stack import Stack
 
 app = Flask(__name__)
+task_stack = Stack()
 
 @app.route('/')
 def index():
     next_task = task_stack.peek()
-    return render_template('index.html')
+    return render_template('index.html', tasks=task_stack.items, next_task=next_task)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form.get('task')
+    if task:
+        task_stack.push(task)
+    return redirect(url_for('index'))
+
+@app.route('/review', methods=['POST'])
+def review_task():
+    task_stack.pop()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
